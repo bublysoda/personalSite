f6e7df6b95e6183df24c93e16453fbd2fa3b3f24
bublysoda <derekrussellbroden@gmail.com>
2026-08-27 16:04:36 -0500

    Updated Skills

--- src/pages/myskills.jsx
+++ src/pages/myskills.jsx
@@ -5,7 +5,7 @@ export default function MySkills(){
     return(
        <div className="page">
             <div className="boxes">
-                <div className="coding">
+                <div className="list">
                     Languages and Technologies:
                         <div className="bulleted">
                             <ul>
@@ -28,25 +28,24 @@ export default function MySkills(){
                             </ul>
                         </div>
                 </div>
-                <div className="coding">
+                <div className="list">
                     Concepts and Practices:
                         <div className="bulleted">
                             <ul>
-                                <li>Git</li>
-                                <li>Github</li>
-                                <li>HTML</li>
-                                <li>CSS</li>
-                                <li>JS</li>
-                                <li>Jest</li>
-                                <li>Node.js</li>
-                                <li>APIs</li>
-                                <li>Webpack</li>
-                                <li>JSON</li>
-                                <li>React</li>
-                                <li>SQL</li>
-                                <li>PostgreSQL</li>
-                                <li>Passport.js</li>
-                                <li>Prisma</li>
+                                <li>Claude Code & GitHub Copilot</li>
+                                <li>AGILE Methodology</li>
+                                <li>Version Control</li>
+                                <li>Code Review</li>
+                                <li>Responsive Design</li>
+                                <li>REST APIs</li>
+                                <li>Component Based Apps</li>
+                                <li>Database Design</li>
+                                <li>Commit Etiquette</li>
+                                <li>Clean Code Principles</li>
+                                <li>Authentication & Authorization</li>
+                                <li>Password Hashing & Salting</li>
+                                <li>Test Driven Development</li>
+                                <li>Asynchronous Programming</li>
                             </ul>
                         </div>
                 </div>
